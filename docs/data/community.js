3e2bd8872c3bf69289b41173fbad5c2f4476fd91
monumentaal <jpm.klink@gmail.com>
2026-03-30 09:26:47 +0200

    Update community.js

--- docs/data/community.js
+++ docs/data/community.js
@@ -1,60 +1,6 @@
-var json_community = {
+{
   "type": "FeatureCollection",
   "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "id": 177478253333,
-        "name": "Johannes de doper",
-        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b256/b256.shtml\" target=\"_blank\">Website</a>",
-        "description": "Kerkstraat 1, 6095ND Baexem"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [5.88178756817279, 51.229081609383144]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "id": 1774782539785,
-        "name": "gemeentehuis",
-        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b676/b676.shtml\" target=\"_blank\">Website</a>",
-        "description": "Raadhuisplen 1, 5944AH Arcen"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [6.179710727179911, 51.474383234377285]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "id": 1774783425463,
-        "name": "OLv Onbevlekt Ontvangen",
-        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b3145/b3145.shtml\" target=\"_blank\">Website</a>",
-        "description": "Aan de kerk 3, 6466AA Amstenrade"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [5.928179920639038, 50.93929756508817]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "id": 1774784992052,
-        "name": "Petrus en Paulus",
-        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b26/b26.shtml\" target=\"_blank\">Website</a>",
-        "description": "Burgemeester Gubbelsplein 1, 5944AK Arcen"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [6.18101277, 51.47535941]
-      }
-    }
-
-
     {
       "type": "Feature",
       "properties": {
@@ -130,8 +76,8 @@ var json_community = {
       "geometry": {
         "type": "Point",
         "coordinates": [
-          6.009563450895693,
-          51.634151741105455
+          6.009676048031344,
+          51.63422584330368
         ]
       }
     },
@@ -182,11 +128,54 @@ var json_community = {
           51.49808914525906
         ]
       }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774816080589,
+        "name": "H. Dionysius (Rozenkerkje)",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b6/b6.shtml\" target=\"_blank\">ðŸ”— Website</a>",
+        "description": "Pastoor Pinckersstraat 3, 6049CN Asselt"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.011530357150877,
+          51.224767549951565
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774816280779,
+        "name": "AZC Baexem Kapel",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b276/b276.shtml\" target=\"_blank\">ðŸ”— Website</a>",
+        "description": "Kasteelweg 2, 6095ND Baexem"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.893414018798223,
+          51.21635156065588
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774816748213,
+        "name": "St Gerlachus",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b3153/b3153.shtml\" target=\"_blank\">ðŸ”— Website</a>",
+        "description": "Dalestraat 2, 6262NL Banholt"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.809768920277709,
+          50.790026963250654
+        ]
+      }
     }
   ]
-
-
-
-    
-  
-};
+}
