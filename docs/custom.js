4c19ad69f73e46b3545799ae8a980838cef1fe35
monumentaal <jpm.klink@gmail.com>
2026-04-03 19:24:52 +0200

    Update custom.js

--- docs/custom.js
+++ docs/custom.js
@@ -27,12 +27,11 @@ window.addEventListener("load", function () {
         infoControl.onAdd = function () {
             var div = L.DomUtil.create('div', 'info-panel');
 
-            div.innerHTML = `
-                <div class="info-header">ℹ️ Toelichting</div>
-                <div class="info-content">
-                    Hier jouw uitleg over de kaart.
-                </div>
-            `;
+            div.innerHTML =
+    '<div class="info-header">ℹ️ Toelichting</div>' +
+    '<div class="info-content">' +
+    'Hier jouw uitleg over de kaart.' +
+    '</div>';
 
             L.DomEvent.disableClickPropagation(div);
             return div;
