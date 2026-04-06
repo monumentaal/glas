0d1a4f56591e4148ef5c8cc4fca109bbf8ab082b
monumentaal <jpm.klink@gmail.com>
2026-04-06 22:22:11 +0200

    Update custom.js

--- docs/custom.js
+++ docs/custom.js
@@ -54,8 +54,8 @@ window.addEventListener("load", function () {
 
     // 🌍 startpositie Nederland
     if (window.map) {
-        map.getView().setCenter(ol.proj.fromLonLat([5.3, 52.2]));
-        map.getView().setZoom(7);
+        map.getView().setCenter(ol.proj.fromLonLat([5.4, 52.15));
+        map.getView().setZoom(8);
     }
 });
 
