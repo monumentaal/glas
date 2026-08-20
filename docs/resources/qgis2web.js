9c821ec3328a1b060b0c98665c06c63edb3ce36d
monumentaal <jpm.klink@gmail.com>
2026-08-20 16:09:57 +0200

    Add files via upload

--- docs/resources/qgis2web.js
+++ docs/resources/qgis2web.js
@@ -12,7 +12,7 @@ var map = new ol.Map({
 });
 
 //initial view - epsg:3857 coordinates if not "Match project CRS"
-map.getView().fit([65176.334635, 6506968.172098, 1217184.867968, 7091100.438764], map.getSize());
+map.getView().fit([158065.457552, 6516214.301264, 1194114.524218, 7100346.567931], map.getSize());
 
 //change cursor
 function pointerOnFeature(evt) {
